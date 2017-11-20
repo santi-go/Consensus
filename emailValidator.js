33ce387774a60a6152b51d16cf6953980aa3d2f2
Manuel Gomez Mateo <creawww@gmail.com>
2017-11-20 13:53:55 +0100

    add index remove

---
+++ emailValidator.js
@@ -0,0 +1,6 @@
+        function emailValidator(mail){
+            let regularExpersion=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            
+            return regularExpersion.test(mail);
+
+        }
