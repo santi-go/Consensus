43b375f008b70c6f042f4bc7bc2fe4e0c6761710
ja_ma <ja_ma_ro@hotmail.com>
2017-11-20 13:59:38 +0100

    Mask character introduction

---
+++ public/js/characterValidator.js
@@ -0,0 +1,25 @@
+function checkCharacter(event){
+  var keynum;
+  var local_part = "[ \@ | A-Z | a-z | 0-9 | \! | \# | \$ | \% | \& | \' | \* | \+ | \- | \/ | \= | \? | \^ | \_ | \` | \{ | \| | \} | \& ]";
+  var domain_part = "[ A-Z | a-z | 0-9 | \. |-]";
+
+  keynum = event.which;
+  input_Email = document.getElementById("emailAddress");
+
+  if (input_Email.value.includes("@")) {
+    if (validateCharacter(domain_part, keynum) == null) {
+      event.preventDefault();
+    }
+  } else {
+    if (validateCharacter(local_part, keynum) == null) {
+      event.preventDefault();
+    }
+  }
+}
+
+function validateCharacter(stringPattern, keynum){
+  var str = String.fromCharCode(keynum);
+  var pattern = new RegExp(stringPattern);
+  var result = pattern.exec(str);
+  return result;
+}
