c875c573cccf5344c4888883ebf90e96516669e4
Interdictor <interdictor.dsg@gmail.com>
2017-11-23 11:17:39 +0100

    Create a box where users can paste the proposal

--- public/js/proposal.js
+++ public/js/proposal.js
@@ -1,9 +1,23 @@
+var Proposal = {
+    text_box: document.getElementById('proposal'),
 
-function removeFormat(proposal) {
-    var text = proposal
+    initialize: function() {
+        document.addEventListener('paste', this.pasteProposal.bind(this));
+        this.text_box.textContent = "Insert your proposal here...";
+    },
 
-    result = text.replace(/<(?:.|\n)*?>/gm, '')
+    pasteProposal: function(event) {
+        var pastedText = event.clipboardData.getData('text');
+        sanitizedText = this.sanitize(pastedText);
 
-    return result;
+        this.text_box.textContent = sanitizedText;
+    },
 
-};
+    sanitize: function(text) {
+        result = text.replace(/<(?:.|\n)*?>/gm, '')
+
+        return result;
+    }
+}
+
+Proposal.initialize();
