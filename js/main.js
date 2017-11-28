e3374735ca8ae2705d50f235ab1e0f195df49d24
matejovski <jermanis.mateja@gmail.com>
2017-11-28 12:40:26 +0100

    simplifying the content structure

--- js/main.js
+++ js/main.js
@@ -122,3 +122,110 @@ var GuestsEmail = {
 
 GuestsEmail.initialize();
 ProposerEmail.initialize();
+
+var Proposal = {
+    text_box: document.getElementById('proposal'),
+
+    initialize: function() {
+        document.addEventListener('paste', this.pasteProposal.bind(this));
+        this.text_box.textContent = "Insert your proposal here...";
+    },
+
+    pasteProposal: function(event) {
+        var pastedText = event.clipboardData.getData('text');
+        sanitizedText = this.sanitize(pastedText);
+
+        this.text_box.textContent = sanitizedText;
+        ProcessBlock.initialize();
+    },
+
+    sanitize: function(text) {
+        result = text.replace(/<(?:.|\n)*?>/gm, '')
+
+        return result;
+    }
+}
+
+var ProcessBlock = {
+    textBlock: document.getElementById('proposal'),
+    text: '',
+    paragraphs: [],
+
+    initialize: function() {
+        this.text = this.textBlock.innerText;
+        this.addBlockTags();
+    },
+
+    addBlockTags: function() {
+        var newBlock = '';
+        this.paragraphs = this.text.split("\n");
+        var lines=this.paragraphs;
+        for (key in lines){
+            var convertedLine = "";
+            var lineInProcess = lines[key].trim();
+            if (lineInProcess == "") {
+                convertedLine = this.addBrTag();
+            } else {
+                convertedLine = this.addParagraphTag(lineInProcess);
+            }
+            newBlock += convertedLine;
+        }
+        this.textBlock.innerText = newBlock;
+    },
+
+    addBrTag: function() {
+        return "<br>\n";
+    },
+
+    addParagraphTag: function(lineInProcess) {
+        return "<p>" + lineInProcess + "</p>\n";
+    }
+}
+
+Proposal.initialize();
+
+
+var InvitedEmails = {
+    container: document.getElementById('invited-emails'),
+
+    deleteEmail: function(mail){
+        stringEmails = this.readInputEmails();
+        arrayEmails = this.parseStringToArray(stringEmails);
+        newArrayEmails = this.deleteElementToArray(mail,arrayEmails);
+        newStringEmails = this.parseArrayToString(newArrayEmails);
+        this.updateInputEmails(newStringEmails);
+    },
+
+    readInputEmails: function(){
+        return this.container.value;
+    },
+
+    parseStringToArray(string){
+        return string.split(',');
+    },
+
+    deleteElementToArray: function(element,arrayElements){
+        var newArray=[];
+        arrayElements.forEach(function(elementArray){
+            if ( element != elementArray ){
+                newArray.push(elementArray);
+            }
+        });
+        return newArray;
+    },
+
+    parseArrayToString(array){
+        return array.toString();
+    },
+
+    updateInputEmails: function(emails){
+        this.container.value = emails;
+    },
+
+    createRemoveButton: function(emailBox, buttonStyle, action) {
+        var removeButton = document.createElement('div');
+        emailBox.appendChild(removeButton).classList.add(buttonStyle);
+        removeButton.textContent = "x";
+        removeButton.addEventListener("click", action);
+    }
+}
