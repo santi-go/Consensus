b7d8823df413ae2f19141d0c33dbc6b8e19a7884
Hector <hectorpadillaizquierdo@gmail.com>
2017-12-13 12:30:17 +0100

    adapt js to new code style

--- src/js/proposal.js
+++ src/js/proposal.js
@@ -2,55 +2,55 @@ export var Proposal = {
   inputContainer: null,
   outputContainer: null,
 
-  initialize: function(inputContainerId) {
-    let container = document.getElementById(inputContainerId);
-    this.inputContainer = container.querySelector('pre');
-    this.inputContainer.textContent = "Insert your proposal here...";
-    this.outputContainer = container.querySelector('output');
-    this.prepareEvents();
+  initialize: function (inputContainerId) {
+    let container = document.getElementById(inputContainerId)
+    this.inputContainer = container.querySelector('pre')
+    this.inputContainer.textContent = 'Insert your proposal here...'
+    this.outputContainer = container.querySelector('output')
+    this.prepareEvents()
   },
 
-  prepareEvents: function() {
-    this.inputContainer.addEventListener('paste', this.pasteProposal.bind(this));
+  prepareEvents: function () {
+    this.inputContainer.addEventListener('paste', this.pasteProposal.bind(this))
   },
 
-  pasteProposal: function(event) {
-    let pastedText = event.clipboardData.getData('text');
-    let text = this.sanitize(pastedText);
-    let newBlock = this.addBlockTags(text);
-    this.outputContainer.innerHTML = newBlock;
+  pasteProposal: function (event) {
+    let pastedText = event.clipboardData.getData('text')
+    let text = this.sanitize(pastedText)
+    let newBlock = this.addBlockTags(text)
+    this.outputContainer.innerHTML = newBlock
   },
 
-  sanitize: function(text) {
-    let result = text.replace(/<(?:.|\n)*?>/gm, '');
-    return result;
+  sanitize: function (text) {
+    let result = text.replace(/<(?:.|\n)*?>/gm, '')
+    return result
   },
 
-  addBlockTags: function(text) {
-    let newBlock = '';
-    let lines = text.split("\n");
-    for (let line of lines){
-      newBlock += this.addTag(line);
+  addBlockTags: function (text) {
+    let newBlock = ''
+    let lines = text.split('\n')
+    for (let line of lines) {
+      newBlock += this.addTag(line)
     }
-    return newBlock;
+    return newBlock
   },
 
-  addTag: function(line) {
-    let convertedLine = "";
-    let lineInProcess = line.trim();
-    if (lineInProcess == "") {
-      convertedLine = this.addBrTag();
+  addTag: function (line) {
+    let convertedLine = ''
+    let lineInProcess = line.trim()
+    if (lineInProcess === '') {
+      convertedLine = this.addBrTag()
     } else {
-      convertedLine = this.addParagraphTag(lineInProcess);
+      convertedLine = this.addParagraphTag(lineInProcess)
     }
-    return convertedLine;
+    return convertedLine
   },
 
   addBrTag: () => {
-    return "<br>\n";
+    return '<br>\n'
   },
 
   addParagraphTag: (lineInProcess) => {
-    return "<p>" + lineInProcess + "</p>\n";
-  },
-};
+    return '<p>' + lineInProcess + '</p>\n'
+  }
+}
