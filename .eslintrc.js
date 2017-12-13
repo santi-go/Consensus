79325bc33a94d7b950455d56261cb9e9a6c2f7a5
Raul Martinez i Peris <raul@cuchame.es>
2017-12-13 13:46:08 +0100

    Added plugins to linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,14 @@ module.exports = {
     "extends": "standard",
     "env": {
       "mocha": true,
-
-    }
+      "webextensions": true,
+      "jquery": true
+    },
+    "rules": {
+        "no-unused-expressions": 0,
+        "chai-friendly/no-unused-expressions": 2
+    },
+    "plugins": [
+        "chai-friendly"
+    ]
 };
