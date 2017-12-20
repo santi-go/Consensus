940b960436a2258548fe1e56ccb425b5d6fb30ea
Raul Martinez i Peris <raul@cuchame.es>
2017-12-20 23:08:43 +0100

    Update server ecosystem

--- system/spec/app_spec.rb
+++ system/spec/app_spec.rb
@@ -1,5 +1,6 @@
 require 'net/http'
 require 'rspec'
+require_relative '../app.rb'
 
 describe 'System' do
   it 'is up' do
@@ -13,10 +14,10 @@ end
 
 describe 'Email' do
   it 'subject has first six words of the proposal' do
-   
+
     proposal = "En mi opinion deberiamos de crear una funcion que determine el uso del mail"
     subject = first_six_words(proposal)
-   
+
     expect(subject).to eq("En mi opinion deberiamos de crear...")
   end
 
@@ -25,39 +26,9 @@ describe 'Email' do
     proposal = "La propuesta esta creada. Consiste en esto."
     proposal1 = "La propuesta esta creada<br>"
     proposal2 = "<p>propuesta</p><p></p><p>con br y p<br> Consiste en esto<p>"
-  
+
     expect(first_six_words(proposal)).to eq("La propuesta esta creada.")
     expect(first_six_words(proposal1)).to eq("La propuesta esta creada...")
     expect(first_six_words(proposal2)).to eq("propuesta...")
   end
 end
-
-
-
-def first_six_words(text)
-
-  text = text.gsub(/<p>/, "")
-
-  if text.include? "</p>" then
-    index_p_end = text.index('</p>')
-    if (index_p_end > 0) then
-      text = text[0, index_p_end]
-    end
-  end
-
-  if text.include? "<br>" then
-    index_break_line = text.index('<br>')
-    if (index_break_line > 0) then text = text[0, index_break_line] end
-  end
-
-  text = text.split[0..5].join(" ")
-
-  if text.include?(".")
-    length_to_dot = text.index(".")
-    return text[0, length_to_dot + 1]
-  else
-      text + "..."
-  end
-end
-
-
